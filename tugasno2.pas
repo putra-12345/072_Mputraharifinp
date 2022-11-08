417c25b259444c6165274b0ba0682cf393f02ddd
muhammad putra harifin pane <92134101+putra-12345@users.noreply.github.com>
2022-11-08 11:15:20 +0700

    Update tugasno2.pas

--- tugasno2.pas
+++ tugasno2.pas
@@ -1,18 +1,18 @@
 program tugasno2modul4;
 uses crt;
 var
-  i,j: integer;
+  p,q: integer;
 begin
   clrscr;
-for i := 1  to 5 do
+for p := 1  to 5 do
 begin
-for j := 1  to i do
-if i mod 2 = 1 then
+for q := 1  to p do
+if p mod 2 = 1 then
 begin
 write('* ');  end
 else
 begin
-  write(i,' ');
+  write(p,' ');
 end;
 writeln;
 end;
